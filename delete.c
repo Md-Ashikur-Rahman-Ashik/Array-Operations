cefba41bfc169ca46430ea4754622807b6463a40
Md-Ashikur-Rahman-Ashik <md.ashikur.upwork@gmail.com>
2025-04-20 17:59:52 +0600

    Deleted a value from array

--- delete.c
+++ delete.c
@@ -16,10 +16,36 @@ int main()
 
     scanf("%d", &modify_index);
 
-    for (j = i - 1; j >= 0; j--)
+    // for (j = i - 1; j >= 0; j--)
+    // {
+    //     /* code */
+    //     printf("%d ", arr[j]);
+    //     // if (j == modify_index)
+    //     // {
+    //     //     printf("0 ");
+    //     // }
+    //     // arr[i] = arr[i + 1];
+
+    //     for (k = 0; k <= j; k++)
+    //     {
+    //         /* code */
+    //         printf("%d ", arr[k]);
+    //     }
+    // }
+
+    for (j = 0; j < array_size - 1; j++)
     {
         /* code */
-        printf("%d ", arr[j]);
+        if (j < modify_index)
+        {
+            /* code */
+            printf("%d ", arr[j]);
+        }
+        else
+        {
+            arr[j] = arr[j + 1];
+            printf("%d ", arr[j]);
+        }
     }
 
     return 0;
