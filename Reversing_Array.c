5806608da8399751030bd2bb151c35548f571228
Md-Ashikur-Rahman-Ashik <md.ashikur.upwork@gmail.com>
2025-04-20 19:54:05 +0600

    Reversed an array

---
+++ Reversing_Array.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+int main()
+{
+    int array_size, i, l;
+
+    scanf("%d", &array_size);
+
+    int arr[array_size];
+
+    for (i = 0; i < array_size; i++)
+    {
+        /* code */
+        scanf("%d", &arr[i]);
+    }
+
+    int j = 0;
+    int k = array_size - 1;
+
+    while (j < k || j != k)
+    {
+        /* code */
+        int tmp = arr[j];
+        arr[j] = arr[k];
+        arr[k] = tmp;
+
+        j++;
+        k--;
+    }
+
+    for (l = 0; l < array_size; l++)
+    {
+        /* code */
+        printf("%d ", arr[l]);
+    }
+
+    return 0;
+}
